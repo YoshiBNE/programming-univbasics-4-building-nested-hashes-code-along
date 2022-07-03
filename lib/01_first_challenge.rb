2f54d9b9e494023a2f70b222a251875d9ade94cf
YoshiBNE <yoshiyasu_express@me.com>
2022-07-04 02:01:10 +1000

    done

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,33 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+      :montague => {},
+      :capulet => {}
+   }
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def first_challenge
+#    epic_tragedy = {
+#       :montague => {},
+#       :capulet => {}
+#    }
+# end
